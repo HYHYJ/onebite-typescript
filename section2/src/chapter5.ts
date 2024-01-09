d4c10e85c4d917af2efdfbfc207d41d4e5bd44c1
HYHYJ <jjangdaram@naver.com>
2024-01-09 18:12:39 +0900

    📝 ch.5 정리

---
+++ section2/src/chapter5.ts
@@ -0,0 +1,33 @@
+//Enum 타입
+// 여러가지 값들에 각가 이름을 부여해 열거해두고 사용하는 타입
+// 자바스크립트엔 없는 새로운 타입
+
+enum Role {
+  ADMIN = 10,
+  USER,
+  GUEST,
+}
+
+enum Language {
+  korean = "ko",
+  english = "en",
+}
+
+const user1 = {
+  name: "이정환",
+  role: Role.ADMIN, // 0 <= 관리자
+  language: Language.korean,
+};
+
+const user2 = {
+  name: "홍길동",
+  role: Role.USER, // 1 <= 일반 유저
+  language: Language.english,
+};
+
+const user3 = {
+  name: "아무개",
+  role: Role.GUEST, // 2 <= 게스트
+};
+
+console.log(user1, user2, user3);
