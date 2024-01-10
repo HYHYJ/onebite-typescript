c1407e863ed1f8a7391c53c34af4c37fa272d0f9
HYHYJ <jjangdaram@naver.com>
2024-01-10 14:56:38 +0900

    📝 ch.2 unknown 정리

---
+++ section3/src/chapter2.ts
@@ -0,0 +1,16 @@
+/**
+ * Unknown 타입
+ */
+
+function UnknownExam() {
+  let a: unknown = 1;
+  let b: unknown = "hello";
+  let c: unknown = true;
+  let d: unknown = null;
+  let e: unknown = undefined;
+
+  let unknownVar: unknown;
+  // let num: number = unknownVar;
+  // let str: string = unknownVar;
+  // let bool: boolean = unknownVar;
+}
