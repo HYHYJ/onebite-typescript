927ee5c199205debf201dadd9a07ba1ed4d46b94
HYHYJ <jjangdaram@naver.com>
2024-01-11 16:03:09 +0900

    📝 ch.2 정리

--- section3/src/chapter2.ts
+++ section3/src/chapter2.ts
@@ -14,3 +14,51 @@ function UnknownExam() {
   // let str: string = unknownVar;
   // let bool: boolean = unknownVar;
 }
+
+/**
+ * Never 타입
+ */
+
+function neverExam() {
+  //반환할 값이 아무것도 없다.
+  function neverFunc(): never {
+    while (true) {}
+  }
+  let num: number = neverFunc();
+  let str: string = neverFunc();
+  let bool: boolean = neverFunc();
+
+  // let never1:never = 10;
+  // let never2:never = "string";
+  // let never3:never = true;
+}
+
+/**
+ *  Void 타입
+ */
+
+function voidExam() {
+  function voidFunc(): void {
+    console.log("hi");
+    return undefined;
+  }
+
+  let voidVar: void = undefined;
+}
+
+/**
+ * any 타입
+ */
+
+function anyExam() {
+  let unknownVar: unknown;
+  let anyVar: any;
+  let undefinedVar: undefined;
+  let neverVar: never;
+
+  anyVar = unknownVar;
+
+  undefinedVar = anyVar;
+
+  // neverVar = anyVar;
+}
